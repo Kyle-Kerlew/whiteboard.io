8d4629babcacb2d2082fbc5575cb01a714b044a1
Kyle Kerlew <kylekerlew1@gmail.com>
2021-03-23 10:18:04 -0400

    resolve structure issue preventing connection to socket

---
+++ server/persistence/connections/mongodb.js
@@ -0,0 +1,26 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://kerlks:***REMOVED***@cluster0.bwkf5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+async function insert(client, db, doc) {
+    return db.collection('DrawingData').insertOne(doc).catch(err => console.log(err)).finally(() => client.close);
+}
+
+async function update(fillQuery, updateQuery, client, db) {
+    return db.collection('DrawingData').updateOne(fillQuery, updateQuery).catch(err => console.log(err)).finally(() => client.close);
+}
+
+client.connect(err => {
+    if (err) throw err;
+    console.log('connected');
+
+})
+module.exports = {
+    mongodb: {
+        insert,
+        update,
+        client
+    }
+}
