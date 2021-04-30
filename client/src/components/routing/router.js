d045304e8824b922190989867ca440a8f55b91fd
Kyle Kerlew <kylekerlew1@gmail.com>
2021-04-29 21:20:23 -0400

    begin building rough home page and nav bar

--- client/src/components/routing/router.js
+++ client/src/components/routing/router.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import Popover from "../toolbar/popover";
+import Home from "../toolbar/home";
 import Canvas from "../drawing/canvas";
 
 function Router() {
     return (
         <Switch>
-            <Route exact path="/" component={Popover}/>
+            <Route exact path="/" component={Home}/>
             <Route path="/:canvasId" component={Canvas}/>
         </Switch>
     )
