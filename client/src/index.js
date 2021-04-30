2250f49ddbbd59eec205beb353757c9f17e0c74d
Kyle Kerlew <kylekerlew1@gmail.com>
2021-04-29 21:20:23 -0400

    begin building rough home page and nav bar

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/navigation/navbar";
+
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <NavBar/>
+                <App/>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
