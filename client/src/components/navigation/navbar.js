fb383927ff7954a8e202555a2cd5f1d8f0991b96
Kyle Kerlew <kylekerlew1@gmail.com>
2021-05-04 11:39:05 -0400

    redo copy text code

--- client/src/components/navigation/navbar.js
+++ client/src/components/navigation/navbar.js
@@ -1,24 +1,16 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 const NavBar = () => {
 
     return (
         <Navbar bg="light" expand="lg" className='fixed-top'>
-            <Navbar.Brand href="#home">Whiteboard IO</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Brand href="/">Whiteboard IO</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+                    <Nav.Link href="/">Home</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- client/src/components/toolbar/bottomtoolbar.js
+++ client/src/components/toolbar/bottomtoolbar.js
@@ -6,16 +6,16 @@ import ZoomInIcon from '../../resources/svg/zoom-in-icon.svg'
 import ZoomOutIcon from '../../resources/svg/zoom-out-icon.svg'
 import Circle from "../svg/circle";
 
-const BottomToolbar = props => {
+const BottomToolbar = ({mouseDown, id, setPaintSize, zoomIn, zoomOut, clearBoard, setIsPopupVisible, isPopupVisible}) => {
 
     const [isActive, setIsActive] = useState(false);
     const toolbarRef = createRef();
     const prevMouseDown = useRef();
-    useEffect(() => prevMouseDown.current = props.mouseDown)
+    useEffect(() => prevMouseDown.current = mouseDown)
 
     useEffect(() => {
 
-        if (props.mouseDown && toolbarRef.current.style.opacity === "0") {
+        if (mouseDown && toolbarRef.current.style.opacity === "0") {
             setTimeout(() => {
                 if (toolbarRef.current)
                     toolbarRef.current.style.display = "none";
@@ -26,10 +26,10 @@ const BottomToolbar = props => {
     });
 
     function getStyle() {
-        if (props.mouseDown) {
+        if (mouseDown) {
             return {opacity: 0, animation: 'fadeOutComplete .5s forwards'}
         }
-        if (prevMouseDown.current && !props.mouseDown) {
+        if (prevMouseDown.current && !mouseDown) {
             return {opacity: 0.5, animation: 'fadeInFromComplete .5s forwards'}
         }
         return !isActive ? {opacity: 0.5, animation: 'fadeOut .5s forwards'} : {
@@ -39,32 +39,32 @@ const BottomToolbar = props => {
     }
 
     return (
-        <div id={props.id} ref={el => toolbarRef.current = el} style={getStyle()} className="bottomtoolbar"
+        <div id={id} ref={el => toolbarRef.current = el} style={getStyle()} className="bottomtoolbar"
              onMouseEnter={() => setIsActive(true)} onMouseLeave={() => setIsActive(false)}>
             <ul>
                 <li>
-                    <Circle onClick={() => props.setPaintSize(5)} width={25} height={25}/>
+                    <Circle onClick={() => setPaintSize(5)} width={25} height={25}/>
                 </li>
                 <li>
-                    <Circle onClick={() => props.setPaintSize(30)} width={50} height={50}/>
+                    <Circle onClick={() => setPaintSize(30)} width={50} height={50}/>
                 </li>
                 <li>
-                    <Circle onClick={() => props.setPaintSize(55)} width={75} height={75}/>
+                    <Circle onClick={() => setPaintSize(55)} width={75} height={75}/>
                 </li>
                 <div className="divider"/>
                 <li>
-                    <img width={"36px"} height={"36px"} onClick={props.zoomIn} src={ZoomInIcon} alt={"Zoom In"}/>
+                    <img width={"36px"} height={"36px"} onClick={zoomIn} src={ZoomInIcon} alt={"Zoom In"}/>
                 </li>
                 <li>
-                    <img width={"36px"} height={"36px"} onClick={props.zoomOut} src={ZoomOutIcon} alt={"Zoom Out"}/>
+                    <img width={"36px"} height={"36px"} onClick={zoomOut} src={ZoomOutIcon} alt={"Zoom Out"}/>
                 </li>
                 <div className="divider"/>
 
                 <li>
-                    <img width={"36px"} height={"36px"} onClick={props.clearBoard} src={CrossIcon} alt={"Clear Board"}/>
+                    <img width={"36px"} height={"36px"} onClick={clearBoard} src={CrossIcon} alt={"Clear Board"}/>
                 </li>
                 <li>
-                    <img width={"36px"} height={"36px"} onClick={() => props.setIsPopupVisible(true)} src={LinkIcon}
+                    <img width={"36px"} height={"36px"} onClick={() => setIsPopupVisible(true)} src={LinkIcon}
                          alt="Get Share Link"/>
                 </li>
             </ul>
