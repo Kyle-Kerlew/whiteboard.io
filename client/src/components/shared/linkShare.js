c4eda6b696125e39dc4184105f15c2a3151f439b
Kyle Kerlew <kylekerlew1@gmail.com>
2021-05-04 11:59:22 -0400

    spruce up copy link design'

--- client/src/components/shared/linkShare.js
+++ client/src/components/shared/linkShare.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../styles/shareLinkBox.css';
-import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField} from '@material-ui/core';
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Snackbar,
+    TextField
+} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import copy from "copy-to-clipboard";
+import Alert from '@material-ui/lab/Alert';
 
-function ShareLinkBox({text, whiteboardId, setIsVisible}) {
+function ShareLinkBox({text, whiteboardId, setIsVisible, showSuccessToast}) {
     const baseurl = `http://localhost:3000/`; //todo: not for prod
+    const [open, setOpen] = useState(false);
 
     function copyLink() {
         const copyText = document.getElementById("value");
         copy(copyText.value);
+        setOpen(true);
     }
 
     return (
@@ -27,11 +38,13 @@ function ShareLinkBox({text, whiteboardId, setIsVisible}) {
                     disabled
                     value={baseurl + whiteboardId}
                 />
+
             </DialogContent>
             <DialogActions>
                 <Button onClick={() => {
                     copyLink();
                     setIsVisible(false);
+                    showSuccessToast();
                 }} color="primary">
                     Copy
                 </Button>
