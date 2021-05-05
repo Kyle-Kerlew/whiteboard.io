65204b80640ebcd69002ca8c16bab2134df2e996
Kyle Kerlew <kylekerlew1@gmail.com>
2021-05-05 15:44:31 -0400

    resolve cors issues

--- server/app.js
+++ server/app.js
@@ -2,19 +2,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const sassMiddleware = require('node-sass-middleware');
-const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "https://whiteboard-io-ui.ue.r.appspot.com/");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    }
-)
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
     src: path.join(__dirname, 'public'),
