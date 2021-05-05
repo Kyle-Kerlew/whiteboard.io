7152ddf6ec83175255ab864eedda40ed6f6e2c24
Kyle Kerlew <kylekerlew1@gmail.com>
2021-05-05 15:44:31 -0400

    resolve cors issues

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.set('port', port);
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://whiteboard-io-ui.ue.r.appspot.com/",
         methods: ["GET", "POST"]
     },
     serveClient: false,
