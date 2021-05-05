e06a478b068b507b8b6de5ab6a2606b80647db08
Kyle Kerlew <kylekerlew1@gmail.com>
2021-05-05 15:24:51 -0400

    configure corsr

--- server/app.js
+++ server/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const sassMiddleware = require('node-sass-middleware');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "https://whiteboard-io-ui.ue.r.appspot.com/");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    }
+)
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(sassMiddleware({
     src: path.join(__dirname, 'public'),
