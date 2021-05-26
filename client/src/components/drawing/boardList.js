acc7b44bcf2ff595eb87edb57f147b9cc3539996
Kyle Kerlew <kylekerlew1@gmail.com>
2021-05-25 21:41:43 -0400

    add eraser functionality

---
+++ client/src/components/drawing/boardList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function BoardList() {
+    // grid display of boards user is a part of
+    async function getBoards() {
+        //call api for boards user is a part of
+    }
+    return (
+        <div className='container'>
+
+        </div>
+    )
+}
+
+export default BoardList;
