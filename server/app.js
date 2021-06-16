8d2154a9c00fc8cbbc1347395a2ef57814bf4e94
Kyle Kerlew <kylekerlew1@gmail.com>
2021-06-16 14:01:33 -0400

    Implement JWT-based authorization

--- server/app.js
+++ server/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const userController = require('./rest/controller/userController');
 const compression = require('compression');
 const cors = require('cors');
+const {authenticationService} = require("./service/authentication/authenticationService");
 
 const expressServer = express();
 
 expressServer.use(express.json());
 expressServer.use(express.urlencoded({extended: false}));
-expressServer.use(cookieParser());
+// expressServer.set('trust proxy', 1); //trust first proxy TODO: wtf does this do?
 expressServer.use(compression());
 expressServer.use(cors());
+expressServer.use('/user/whiteboards', authenticationService.authenticateUser);
 expressServer.use('/user', userController);
 
 
--- server/persistence/user/userPersistence.js
+++ server/persistence/user/userPersistence.js
@@ -2,21 +2,31 @@ async function createUser(collection, user) {
     try {
         return await collection.insertOne(user);
     } catch (e) {
-        console.log("Error inserting data", e)
+        console.log("Error inserting data", e);
+        throw "Error creating user";
     }
 }
 
-async function findUserByEmail(collection, userId) {
+async function findUserByEmail(collection, email) {
     try {
-        return await collection.findOne({email: userId});
+        return await collection.findOne({email});
     } catch (e) {
         console.log("Error finding user", e);
+    }
+}
+
+async function findUserById(collection, userId) {
+    try {
+        return await collection.findOne({_id: userId});
+    } catch (e) {
+        console.log("Error finding user by id");
     }
 }
 
 module.exports = {
     UserPersistence: {
         createUser,
-        findUser: findUserByEmail
+        findUserByEmail,
+        findUserById
     }
 }
