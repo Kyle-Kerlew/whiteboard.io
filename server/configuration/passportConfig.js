b75df3ccfbd16167d27516658cb2b437d4a91f5a
Kyle Kerlew <kylekerlew1@gmail.com>
2021-06-18 19:48:35 -0400

    implement cookie-based sessions

--- server/configuration/passportConfig.js
+++ server/configuration/passportConfig.js
@@ -1,31 +1,48 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const CookieStrategy = require('passport-cookie');
 const {authenticationService} = require('../service/authentication/authenticationService');
 const {userService} = require('../service/user/userService');
 
 
-passport.use('local', new LocalStrategy({},
-    async function (email, password, done) {
+passport.use(new CookieStrategy({
+        cookieName: 'session-id',
+        passReqToCallback: true
+    },
+    async function (cookie, token, done) {
         try {
-            const response = await authenticationService.verifyPassword({email, password});
-            return done(null, response);
+            const user = await userService.findUserByEmail(cookie.session.passport.user);
+            if (!user) {
+                return done(null, false);
+            }
+            return done(null, user);
         } catch (e) {
-            return done(null, false, {error: 'Invalid Credentials'});
-
+            return done(e);
         }
     }));
+passport.use(new LocalStrategy(async function (email, password, done) {
+    try {
+        const response = await authenticationService.verifyPassword({email, password});
+        return done(null, response);
+    } catch (e) {
+        return done(e);
+
+    }
+}));
 
 passport.serializeUser(function (user, done) {
-    console.log(user);
     done(null, user.email);
 });
 
 passport.deserializeUser(async function (email, done) {
     try {
         const userEntity = await userService.findUserByEmail(email);
-        done(undefined, userEntity);
+        if (!userEntity) {
+            return done('No user found', userEntity);
+        }
+        return done(null, userEntity);
     } catch (e) {
-        done(e, undefined);
+        return done(e);
     }
 
 });
