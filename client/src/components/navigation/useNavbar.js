2a93a60493bde08cd9e31db02bd2315d8fb7f77f
Kyle Kerlew <kylekerlew1@gmail.com>
2021-06-22 00:26:08 -0400

    implement basic structure of active users component

--- client/src/components/navigation/useNavbar.js
+++ client/src/components/navigation/useNavbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import "../../styles/navBar.css"
 
-const useNavbar = NavigationOptionsComponent => {
+const useNavbar = (NavigationOptionsComponent, style) => {
     const Result = () => (
-        <Navbar bg="light" expand="lg" className='fixed-top'>
+        <Navbar bg="light" expand="lg" className={'fixed-top' + ` ${style}`}>
             <Navbar.Brand href="/">Whiteboard IO</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
