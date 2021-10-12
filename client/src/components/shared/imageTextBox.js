2bbf272ca7ee1ff37deafa3d423834cce3b1927a
Kyle Kerlew <kylekerlew1@gmail.com>
2021-10-12 01:09:20 -0400

    Begin redesign updates

---
+++ client/src/components/shared/imageTextBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/imageTextBox.css';
+
+function ImageTextBox() {
+    return (
+        <div className={'image-text-box'}>
+            {children}
+        </div>
+    )
+}
+
+export default ImageTextBox;
