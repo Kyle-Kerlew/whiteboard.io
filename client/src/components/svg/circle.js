89bc3679bd0d2a67a1f2b4b02aa3e0649d7dc085
Kyle Kerlew <kylekerlew1@gmail.com>
2021-11-24 00:12:01 -0500

    Tweak visuals with padding on the home page

--- client/src/components/svg/circle.js
+++ client/src/components/svg/circle.js
@@ -1,9 +1,10 @@
+import React from 'react';
+import CircleIcon from '../../resources/svg/circle-icon.svg';
+
 const Circle = ({size, onClick, color}) => {
     return (
         <div>
-            <svg onClick={onClick} width={size} height={size} xmlns="http://www.w3.org/2000/svg">
-                <circle cx={size / 2} cy={size / 2} r={size / 2} fill={color} stroke="none"/>
-            </svg>
+            <img src={CircleIcon}/>
         </div>
     )
 }
