bfa58d1972c418f0cd8d2afae44091122a00aab1
Kyle Kerlew <kylekerlew1@gmail.com>
2021-12-26 15:21:51 -0500

    Resolve failure to parse jsx

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/globals.css'
-import {BrowserRouter} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Router from "./components/routing/router";
 import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import Router from './components/routing/router';
 import store from './configuration/store';
+import './styles/globals.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <Router/>
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
