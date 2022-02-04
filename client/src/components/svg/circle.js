e7d5b0e75dfebea631c8269a781b49a313ad7b6f
Kyle Kerlew <kylekerlew1@gmail.com>
2022-02-04 18:48:57 -0500

    move drawing functionality into its own class out of canvas

--- client/src/components/svg/circle.js
+++ client/src/components/svg/circle.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Circle = ({size, onClick, color}) => {
-    return (
-        <svg height={size} onClick={onClick} width={size} xmlns="http://www.w3.org/2000/svg">
-            <circle cx={size/2} cy={size/2} r=".1" stroke={color} strokeWidth={size} fill={color}/>
-        </svg>
-    )
-}
+const Circle = ({
+  size,
+  onClick,
+  color,
+}) => {
+  return (
+    <svg height={size} onClick={onClick} width={size} xmlns='http://www.w3.org/2000/svg'>
+      <circle cx={size / 2} cy={size / 2} fill={color} r='.1' stroke={color} strokeWidth={size} />
+    </svg>
+  );
+};
 
 export default Circle;
