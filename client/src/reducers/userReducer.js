86d6b8673e9ac8f4f96a7c5a7555fc7897d55c17
Kyle Kerlew <kylekerlew1@gmail.com>
2022-05-08 23:58:06 -0400

    Resolve sonar lint warnings. Use arrow function consistently instead of function keyword. remove useless useCallback hooks

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,29 +1,36 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {
+  createSlice,
+} from '@reduxjs/toolkit';
 
 export const userSlice = createSlice(
-    {
-        name: 'user',
-        initialState: {
-            value: {
-                isLoadingUser: true,
-                isAuthenticated: false,
-                role: '',
-            }
-        },
-        reducers: {
-            loginUser: (state, action) => {
-                state.value.isAuthenticated = action.payload !== 'guest';
-                state.value.role = action.payload;
-            },
-            logoutUser: state => {
-                state.value.isAuthenticated = false;
-                state.value.role = '';
-            },
-            finishLoadingUser: state => {
-                state.value.isLoadingUser = false;
-            }
-        }
-    });
-export const {loginUser, logoutUser, finishLoadingUser} = userSlice.actions;
+  {
+    initialState: {
+      value: {
+        isAuthenticated: false,
+        isLoadingUser: true,
+        role: '',
+      },
+    },
+    name: 'user',
+    reducers: {
+      finishLoadingUser: (state) => {
+        state.value.isLoadingUser = false;
+      },
+      loginUser: (state, action) => {
+        state.value.isAuthenticated = action.payload !== 'guest';
+        state.value.role = action.payload;
+      },
+      logoutUser: (state) => {
+        state.value.isAuthenticated = false;
+        state.value.role = '';
+      },
+    },
+  },
+);
+export const {
+  loginUser,
+  logoutUser,
+  finishLoadingUser,
+} = userSlice.actions;
 
 export default userSlice.reducer;
